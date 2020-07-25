daa351227a4bea2b9720a55db4c9df59471ced68
Jose luis San martin <joseluis@MacBook-Air-de-Jose.local>
2020-07-25 01:47:05 -0400

    Se realizaron cambios de funcionalidad

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,17 @@
 export function login(usuario){
 
-  var url = 'http://localhost:5000/inicio'
+  var url = 'http://localhost:5000/registro'
   var datos = {
   email: usuario.email,
-  password: usuario.password
+  password: usuario.password,
+  nombre:'buena carlos',
+  fecha:'hoy'
+
 };
 
   fetch( url , {
     method: 'POST',
-    body : JSON.stringify({ datos }),
+    body : JSON.stringify( datos ),
     headers: {
       'Content-Type' : 'application/json'
     }
